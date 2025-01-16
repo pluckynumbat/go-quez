04f5a5953c9947f4e30ef7ae16a71d0e60c24b3f
Neehar Patil <pluckynumbat@gmail.com>
2025-01-16 10:59:06 +0530

    added basic state check methods for the semi generic queue

--- sgquezlib/sgquezlib.go
+++ sgquezlib/sgquezlib.go
@@ -10,3 +10,18 @@ import (
 type SemiGenericQueue[T fmt.Stringer] struct {
 	sdlist *sdlistlib.SemiGenericList[T]
 }
+
+// IsNil checks whether a pointer to a Semi Generic Queue is nil
+func (queue *SemiGenericQueue[T]) IsNil() bool {
+	return queue == nil
+}
+
+// Internal Method to check whether the underlying list is nil
+func (queue *SemiGenericQueue[T]) isListNil() bool {
+	return queue.IsNil() || queue.sdlist.IsNil()
+}
+
+// IsEmpty checks whether a Semi Generic Queue is empty
+func (queue *SemiGenericQueue[T]) IsEmpty() bool {
+	return queue.IsNil() || queue.isListNil() || queue.sdlist.IsEmpty()
+}
